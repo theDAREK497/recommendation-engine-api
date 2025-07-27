7c1d10379c215943512f508e2f8db29caae2283a
Ilya <ilion9871@gmail.com>
2025-07-28 01:32:09 +0300

    WIP Tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,7 @@
-import pytest
-from fastapi.testclient import TestClient
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 from app.main import app
 
 client = TestClient(app)
